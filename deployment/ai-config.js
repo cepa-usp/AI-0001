197438dd47a4e6531363973dc927a82144742823
Alexandre <aledrones@hotmail.com>
2014-09-03 15:39:03 -0300

    Inserção do código do google analytics

--- deployment/ai-config.js
+++ deployment/ai-config.js
@@ -1,3 +1,19 @@
+/*
+ * Configuração do Google Analytics
+ */
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-33114043-1']); // Tracking ID do Atividades Interativas
+_gaq.push(['_trackPageview']);
+
+(function() {
+	var ga = document.createElement('script');
+	ga.type = 'text/javascript';
+	ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
+})();
+
 // JavaScript Document
 var flashvars = {};
 flashvars.ai = "AI-0001.swf";
